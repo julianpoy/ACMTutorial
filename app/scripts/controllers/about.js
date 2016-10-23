86223997e116ac55da025c9da3d1e613144b7526
Julian Poyourow <julianpoyo@gmail.com>
2016-10-23 16:29:57 -0700

    Intialized the project with Yeoman

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name acmtutorialApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the acmtutorialApp
+ */
+angular.module('acmtutorialApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
