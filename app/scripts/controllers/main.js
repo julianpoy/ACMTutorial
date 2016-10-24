c0f6dd1a0059727167ea73966f94a57e6dc0c128
Julian Poyourow <julianpoyo@gmail.com>
2016-10-24 15:01:20 -0700

    Awesome task manager with momentjs

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -15,9 +15,14 @@ angular.module('acmtutorialApp')
     $scope.tasks = [];
 
     $scope.createNewTask = function(){
+        $scope.newTask.created = moment().subtract(5, "minutes").format("MM/DD/YYYY hh:mm:ss");
         $scope.tasks.push($scope.newTask);
         
         $scope.newTask = {};
+    }
+    
+    $scope.deleteTask = function(taskIndex){
+        $scope.tasks.splice(taskIndex, 1);
     }
 
     
