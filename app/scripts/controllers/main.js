afd24905690ac02905ae92796a27abbe48d10c34
Julian Poyourow <julianpoyo@gmail.com>
2016-10-24 14:27:38 -0700

    Basic angular tutorial code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,14 @@
  * Controller of the acmtutorialApp
  */
 angular.module('acmtutorialApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('MainCtrl', function ($scope) {
+    $scope.example = "ABC123";
+    
+    $scope.myFunction = function(){
+        return $scope.example.length > 9;
+    }
+    
+    $scope.addTwo = function(){
+        $scope.example += "2";
+    }
   });
